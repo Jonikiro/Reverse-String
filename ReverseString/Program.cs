e1c1bf18b9113995681647c688ef8c75d1ffd0a4
Jonikiro <mcabrera122@gmail.com>
2018-04-18 10:25:26 -0500

    Final Build

---
+++ ReverseString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string to be reversed: ");
+            string oldString = Console.ReadLine();
+            char[] newString = oldString.ToCharArray();
+            Array.Reverse(newString);
+            oldString = String.Join("", newString);
+            Console.WriteLine("\nAnd now, the reversed string...\n");
+            Console.WriteLine(oldString);
+            Console.ReadKey();
+        }
+    }
+}
