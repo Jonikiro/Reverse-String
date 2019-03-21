54d9a8e9f62ff4a6ab31a552f078e024390d4232
Jonikiro <mcabrera122@gmail.com>
2019-03-21 11:12:40 -0500

    Added reset option

--- ReverseString/Program.cs
+++ ReverseString/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,13 +11,30 @@ namespace ReverseString
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a string to be reversed: ");
-            string oldString = Console.ReadLine();
-            char[] newString = oldString.ToCharArray();
-            Array.Reverse(newString);
-            oldString = String.Join("", newString);
-            Console.WriteLine("\nAnd now, the reversed string...\n");
-            Console.WriteLine(oldString);
+            string choice;
+            while (true)
+            {
+                Console.Write("Enter a string to be reversed: ");
+                string oldString = Console.ReadLine();
+                char[] newString = oldString.ToCharArray();
+                Array.Reverse(newString);
+                oldString = String.Join("", newString);
+                Console.WriteLine("\nAnd now, the reversed string...\n");
+                Console.WriteLine(oldString);
+                Console.Write("\nWould you like to try again? Enter 'yes' or 'no': ");
+                choice = Console.ReadLine();
+                choice.ToLower();
+                while (choice != "yes" && choice != "no")
+                {
+                    Console.Write("\nInvalid input. Would you like to try again? Enter 'yes or 'no': ");
+                    choice = Console.ReadLine();
+                }
+
+                Console.Write("\n");
+                if (choice == "yes") continue;
+                else if (choice == "no") break;
+            }
+            Console.Write("Thank you for using this program.");
             Console.ReadKey();
         }
     }
